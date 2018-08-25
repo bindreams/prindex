ac91cc29ab2fdee42bae368bdc9fad61437fc2e5
Anna Zhukova <28830446+bindreams@users.noreply.github.com>
2018-08-25 18:28:25 +0300

    Added more tests

--- prindex/main.cpp
+++ prindex/main.cpp
@@ -18,7 +18,7 @@ class BadDerivedClass : public BadBaseClass {};
 struct GoodBaseClass { virtual ~GoodBaseClass() = default; };
 struct GoodDerivedClass : public GoodBaseClass {};
 
-#define TEST(string, ...) {\
+#define TEST_PRID(string, ...) {\
 	if (strcmp(PRID(__VA_ARGS__).name(), string) == 0) { \
 		cout << "Passed: "; \
 		cout << "tested " << #__VA_ARGS__ << ", got " << string << endl; \
@@ -27,6 +27,16 @@ struct GoodDerivedClass : public GoodBaseClass {};
 		cout << "Failed: "; \
 		cout << "tested " << #__VA_ARGS__ << ", expected " << string << ", got " << PRID(__VA_ARGS__).name() << endl; \
 	} \
+}
+
+#define TEST(...) { \
+	if (__VA_ARGS__) { \
+		cout << "Passed: "; \
+	} \
+	else { \
+		cout << "Failed: "; \
+	} \
+	cout << "tested " << #__VA_ARGS__ << endl; \
 }
 
 int main() {
@@ -40,28 +50,42 @@ int main() {
 #endif
 	cout << endl;
 
-	// Built-in types
-	TEST("int", int);
-	TEST("float", float);
-	TEST("double", double);
-	TEST("char", char);
-	TEST("bool", bool);
+	// Equality and hash codes
+	TEST(PRID(int) == PRID(int));
+	TEST(PRIDX(int) == PRIDX(int));
 
-	TEST("int*", int*);
-	TEST("int**", int**);
+	TEST(PRID(int) != PRID(double));
+	TEST(PRIDX(int) != PRIDX(double));
+
+	TEST(PRID(int).hash_code() == 3661752227);
+	TEST(PRID(float).hash_code() == 3799042738);
+	TEST(PRID(double).hash_code() == 3477808853);
+	TEST(PRID(char).hash_code() == 158743409);
+	TEST(PRID(bool).hash_code() == 3495105888);
+
+	// Names
+	// Built-in types
+	TEST_PRID("int", int);
+	TEST_PRID("float", float);
+	TEST_PRID("double", double);
+	TEST_PRID("char", char);
+	TEST_PRID("bool", bool);
+
+	TEST_PRID("int*", int*);
+	TEST_PRID("int**", int**);
 
 	// Standard library
-	TEST("std::basic_string<char, std::char_traits<char>, std::allocator<char> >", std::string);
-	TEST("std::map<int, double, std::less<int>, std::allocator<std::pair<int const, double> > >", std::map<int, double>);
+	TEST_PRID("std::basic_string<char, std::char_traits<char>, std::allocator<char> >", std::string);
+	TEST_PRID("std::map<int, double, std::less<int>, std::allocator<std::pair<int const, double> > >", std::map<int, double>);
 
 	// Weird Behaviour
-	TEST("int", const int&);
+	TEST_PRID("int", const int&);
 
 	// Namespaces and templates
-	TEST("name_space::MyClass", name_space::MyClass);
-	TEST("TemplateStruct<int>", TemplateStruct<int>);
-	TEST("TemplateStruct<TemplateStruct<int> >", TemplateStruct<TemplateStruct<int>>);
-	TEST("bool", bool);
+	TEST_PRID("name_space::MyClass", name_space::MyClass);
+	TEST_PRID("TemplateStruct<int>", TemplateStruct<int>);
+	TEST_PRID("TemplateStruct<TemplateStruct<int> >", TemplateStruct<TemplateStruct<int>>);
+	TEST_PRID("bool", bool);
 
 	// Variables
 	int int_var = 42;
@@ -70,9 +94,9 @@ int main() {
 	BadBaseClass* non_polymorphic_var = new BadDerivedClass;
 	GoodBaseClass* polymorphic_var = new GoodDerivedClass;
 
-	TEST("int", int_var);
-	TEST("name_space::MyClass", class_var);
-	TEST("TemplateStruct<double>", template_var);
-	TEST("BadBaseClass", *non_polymorphic_var);
-	TEST("GoodDerivedClass", *polymorphic_var);
+	TEST_PRID("int", int_var);
+	TEST_PRID("name_space::MyClass", class_var);
+	TEST_PRID("TemplateStruct<double>", template_var);
+	TEST_PRID("BadBaseClass", *non_polymorphic_var);
+	TEST_PRID("GoodDerivedClass", *polymorphic_var);
 }
